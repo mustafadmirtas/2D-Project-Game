3911ceabdcd996cedf36bb607c94d5545b850966
mustafa demirtaş <mustafa.demirtas@spinmatic.com>
2021-03-23 00:46:39 +0300

    movement update

--- Assets/CharacterController2D.cs
+++ Assets/CharacterController2D.cs
@@ -9,7 +9,10 @@ public class CharacterController2D : MonoBehaviour
     public float _jumpForce = 2;
     public Animator _animator;
     private float _inputX = 0f;
-    public bool _onGrounded = true;
+    private bool _onGrounded = true;
+    private bool _isAttacking = false;
+    private bool _isRolling = false;
+    private bool _isFaceRight = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,51 +24,79 @@ public class CharacterController2D : MonoBehaviour
     void Update()
     {
         Movement();
+        Jump();
+        Roll();
+        Attack();
     }
     public void Movement()
     {
         //taking input from user
         _inputX = Input.GetAxis("Horizontal");
 
-        // creating movement vector
-        Vector3 movement = new Vector3(_speed.x * _inputX, 0, 0);
-        // 
-        movement *= Time.deltaTime;
+        _animator.SetFloat("speedX", Mathf.Abs(_inputX));
+        if (_inputX != 0 && (!_isAttacking || !_onGrounded) && !_isRolling)
+        {
+            // creating movement vector
+            Vector3 movement = new Vector3(_speed.x * _inputX, 0, 0);
+            // 
+            movement *= Time.deltaTime;
 
-        // changing position of character
-        transform.Translate(movement);
-
+            // for character rotation
+            transform.localScale = new Vector3(_inputX < 0 ? (-5) : 5, 5, 5);
+            _isFaceRight = _inputX < 0 ? false : true;
+            // changing position of character
+            transform.Translate(movement);
+        }
+    }
+    private void Jump()
+    {
         // Jump movement
-        if (Input.GetButtonDown("Jump") && Mathf.Abs(_rb.velocity.y) < 0.001f)
+        if (Input.GetButtonDown("Jump") && _onGrounded && !_isRolling)
         {
             _animator.SetBool("isJumping", true);
             _rb.AddForce(new Vector2(0, _jumpForce), ForceMode2D.Impulse);
-
+            // this is for jump animation if it on air
             _animator.SetFloat("speedX", 0);
         }
-        // this is for jump animation if it is not air
-        if (Mathf.Abs(_rb.velocity.y) < 0.001f)
+    }
+    private void Roll()
+    {
+        if (Input.GetButtonDown("Roll") && _onGrounded && Mathf.Abs(_inputX) > 0)
         {
-            _animator.SetFloat("speedX", Mathf.Abs(_inputX));
+            _isRolling = true;
+            _animator.SetTrigger("roll");
         }
-        // for character rotation
-        if (_inputX >= 0.01f)
-        {
-            transform.localScale = new Vector3(5, 5, 5);
-        } else if (_inputX <= -0.01f)
-        {
-            transform.localScale = new Vector3(-5, 5, 5);
-        }
+
+    }
+
+    private void Attack()
+    {
         // attack animation
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetButtonDown("Fire1"))
         {
+            _isAttacking = true;
             _animator.SetTrigger("attackEnemy");
         }
     }
-    private void OnCollisionEnter(Collision collision)
+
+    private void AttackComplete()
     {
-        if (collision.gameObject.layer == 6)
+        _isAttacking = false;
+    }
+
+    private void RollComplete()
+    {
+        _isRolling = false;
+    }
+    private void RollStart()
+    {
+        _rb.AddForce(new Vector2(_isFaceRight ? 5f : -5f, 0), ForceMode2D.Impulse);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == 6 && !_onGrounded)
         {
+            _animator.SetBool("isJumping", false);
             _onGrounded = true;
         }
     }
